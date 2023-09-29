6d35828541d8a0f55ba4967cfb3a0c2d00b35727
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-09-29 14:23:18 +0900

    Design: LoginForm ui 수정
    1. 요소간 간격 수정
    2. 모바일 반응형 추가

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,16 +7,20 @@ function Login() {
     return (
         <LoginForm>
             <h1>PreOn Connect</h1>
-            <Input type='text' placeholder='사용자 ID 또는 이메일'/>
-            <Input type='password' placeholder='비밀번호'/>
+            <div className='inputs'>
+                <Input type='text' placeholder='사용자 ID 또는 이메일'/>
+                <Input type='password' placeholder='비밀번호'/>
+            </div>
             <ul className='signUpFindPw'>
                 <li>회원 가입</li>
                 <li>&#124;</li>
                 <li>비밀번호 찾기</li>
             </ul>
-            <Button $fill='primary'>로그인</Button>
-            <Button >Google 계정으로 로그인</Button>
-            <Button >카카오 계정으로 로그인</Button>
+            <div className='logins'>
+                <Button $fill='primary'>로그인</Button>
+                <Button >Google 계정으로 로그인</Button>
+                <Button >카카오 계정으로 로그인</Button>
+            </div>
         </LoginForm>
     );
 }
--- src/components/LoginStyle.ts
+++ src/components/LoginStyle.ts
@@ -4,53 +4,87 @@ import styled from "styled-components";
 
 export const LoginForm = styled.div`
   width: 26.625rem;
-  height: 39.063rem;
-  margin: 2rem auto;
+  margin: 4rem auto;
   border: 1px solid ${({theme}) => theme.color.gray_light_5};
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 4.25rem 2.875rem;
-  & > :first-child{
-    margin-bottom:3rem;
-  }
-  & > :not(:first-child):not(:last-child) {
-    margin-bottom:1.5rem;
-  }
-  @media ${({theme}) => theme.screenSize.mobile} {
-    border: none;
-  }
-
-  @media ${({theme}) => theme.screenSize.tablet} {
-  }
-
-  @media ${({theme}) => theme.screenSize.pc} {
-
-  }
 
   & h1 {
     color: ${({theme}) => theme.color.primary};
-    margin-bottom:3rem;
+    margin-bottom: 3rem;
+  }
+
+  & > .inputs, & > .logins {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  & > .inputs {
+    margin-bottom:1.2rem;
   }
   
-  & .signUpFindPw{
-    width:100%;
-    padding:0 3.3rem;
-    display: flex;
-    justify-content: space-around;
-    font-size:.9rem;
-    & li {
-      list-style: none;
-      color:${({theme}) => theme.color.gray_dark_1};
-      &:not(:nth-of-type(2)){
-        cursor:pointer;
-        color:${({theme}) => theme.color.gray_dark_4};
-        &:hover{
-          color:${({theme}) => theme.color.primary};
-          font-weight:600;
-        }
-      }
-      
+  & > .inputs > * {
+    width: 19.750rem;
+    height: 3.5rem;
+  
+  }
+ 
+  & > .inputs > :not(:last-child) {
+    margin-bottom: 1.2rem;
+  }
+
+  & > .logins {
+    margin-top:2.4rem;
+    & > * {
+      width: 19.750rem;
+      height: 3.5rem;
+    }
+    & > :not(:last-child){
+      margin-bottom:1.2rem;
     }
   }
+
+  & .signUpFindPw {
+    width: 100%;
+    padding: 0 3.45rem;
+    display: flex;
+    justify-content: space-around;
+    font-size: .9rem;
+
+    & li {
+      list-style: none;
+      color: ${({theme}) => theme.color.gray_dark_1};
+
+      &:not(:nth-of-type(2)) {
+        cursor: pointer;
+        color: ${({theme}) => theme.color.gray_dark_4};
+
+        &:hover {
+          color: ${({theme}) => theme.color.primary};
+          font-weight: 600;
+        }
+      }
+
+    }
+  }
+
+  @media ${({theme}) => theme.screenSize.mobile} {
+    border:none;
+    & > .inputs > * {
+      width: 17rem;
+      height: 2.5rem;
+    }
+
+    & > .logins > * {
+      width: 17rem;
+      height: 2.8rem;
+    }
+  }
+
+ 
+
+ 
 `;
