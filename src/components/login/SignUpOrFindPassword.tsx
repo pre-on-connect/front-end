30a32758fe4bdb9bad732d929db12a8817c545e4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-02 13:19:19 +0900

    Perf: react-icons삭제, @react-icons/all-files 패키지 설치

--- src/components/login/SignUpOrFindPassword.tsx
+++ src/components/login/SignUpOrFindPassword.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 import Modal from "@/components/ui/Modal";
 import styled from "styled-components";
 import {Button} from "@/components/ui/Button";
-import {FcGoogle} from "react-icons/fc";
-import {RiKakaoTalkFill} from "react-icons/ri";
 import {NavButton} from "@/components/ui/NavButton";
+import {Link} from "@chakra-ui/next-js";
+import {FcGoogle} from "@react-icons/all-files/fc/FcGoogle";
+import {RiKakaoTalkFill} from "@react-icons/all-files/ri/RiKakaoTalkFill";
 
 const StyledSignUpOption = styled.div`
   display: flex;
@@ -39,7 +40,7 @@ function SignUpOrFindPassword() {
                         <StyledSignUpOption>
                             <Button><FcGoogle className='icon'/> Google 계정으로 가입</Button>
                             <Button><RiKakaoTalkFill className='icon'/>카카오 계정으로 가입</Button>
-                            <Button>이메일로 가입</Button>
+                            <Link href='/signUp' color='gray_dark_4' bgColor='gray_light_1' >이메일로 가입</Link>
                         </StyledSignUpOption>
                     </Modal.Window>
                 </li>
