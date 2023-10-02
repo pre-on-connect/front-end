f25acb7dae3b2b452a648c5fcf7b0717ad32c673
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-02 12:32:08 +0900

    Design: Login.tsx 수정

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,13 +4,17 @@ import {Button} from "@/components/ui/Button";
 import {FcGoogle} from "react-icons/fc";
 import {RiKakaoTalkFill} from "react-icons/ri";
 import SignUpOrFindPassword from "@/components/login/SignUpOrFindPassword";
+import {Box} from "@chakra-ui/react";
+import React from "react";
 
 
 function Login() {
     return (
 
         <LoginForm>
-                <h1>PreOn Connect</h1>
+                <Box color="primary" fontSize="32px" fontWeight="semibold" mb="100px">
+                    PreOn Connect
+                </Box>
                 <div className='inputs'>
                     <Input type='text' placeholder='사용자 ID 또는 이메일'/>
                     <Input type='password' placeholder='비밀번호'/>
