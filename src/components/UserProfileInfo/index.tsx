e95751c01fd701bfef7afdf4bca87935ab0ac189
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-04 22:44:23 +0900

    Design: UserProfileInfo ui 정렬 수정

--- src/components/UserProfileInfo/index.tsx
+++ src/components/UserProfileInfo/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import {Avatar, Box, Button, Flex, Image, Stack} from "@chakra-ui/react";
+import {Avatar, Box, Button, Flex, Image, Stack, VStack} from "@chakra-ui/react";
 import {FiLogOut} from "@react-icons/all-files/fi/FiLogOut";
 
 function UserProfileInfo() {
@@ -25,7 +25,7 @@ function UserProfileInfo() {
                         height={['6rem', '8rem', '12rem']}
                         cursor="pointer"/>
                 )}
-                <Flex flexBasis='20rem' flexDirection='column' align='start' justifyContent='space-between'>
+                <VStack flexBasis='20rem' align='start' spacing={6} justify='center'>
                     <Stack direction='row' spacing={4} align='center'>
                         <Box fontSize='1.2rem' fontWeight='bold' >test_id</Box>
                         <Button variant='primary-basic' size='sm' >프로필 편집</Button>
@@ -35,17 +35,17 @@ function UserProfileInfo() {
                     </Stack>
                     <Flex align='center' justifyContent='space-between'>
                         <Box color='gray_dark_4' pr='1rem'>
-                            게시물 <strong>4</strong>
+                            게시물<strong style={{marginLeft:'.25rem'}}>4</strong>
                         </Box>
                         <Button variant='trans-basic'>
-                            팔로워 <strong>4</strong>
+                            팔로워<strong style={{marginLeft:'.25rem'}}>4</strong>
                         </Button>
                         <Button variant='trans-basic'>
-                            팔로잉 <strong>4</strong>
+                            팔로잉<strong style={{marginLeft:'.25rem'}}>4</strong>
                         </Button>
                     </Flex>
-                    <Button variant='tertiary-basic'>회원 탈퇴</Button>
-                </Flex>
+                    <Button variant='tertiary-basic' size='sm'>회원 탈퇴</Button>
+                </VStack>
             </Flex>
     );
 }
