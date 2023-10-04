61917d7c4774aa7fa2072fd179b58b326ead039f
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-04 22:35:03 +0900

    Design: UserProfileInfo ui 정렬

--- src/components/UserProfileInfo/index.tsx
+++ src/components/UserProfileInfo/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import {Avatar, Box, Button, Flex, Image} from "@chakra-ui/react";
+import {Avatar, Box, Button, Flex, Image, Stack} from "@chakra-ui/react";
 import {FiLogOut} from "@react-icons/all-files/fi/FiLogOut";
 
 function UserProfileInfo() {
@@ -8,7 +8,7 @@ function UserProfileInfo() {
     // todo - 사용자정보 조회 코드 추가
     const profileImage = null;
     return (
-            <Flex>
+            <Flex justifyContent='space-around' p='3rem 2rem'>
                 {profileImage ? (
                     <Image
                         src={profileImage}
@@ -25,27 +25,26 @@ function UserProfileInfo() {
                         height={['6rem', '8rem', '12rem']}
                         cursor="pointer"/>
                 )}
-                <Flex flexDirection='column'>
-                    <Flex>
-                        <Box>test_id</Box>
-                        <Button bgColor='primary' color='white'>프로필 편집</Button>
-                        <Flex color='tertiary' cursor='pointer'>
-                            <FiLogOut/>
-                            <Box>로그아웃</Box>
-                        </Flex>
-                    </Flex>
-                    <Flex>
-                        <Box color='gray_dark_4'>
+                <Flex flexBasis='20rem' flexDirection='column' align='start' justifyContent='space-between'>
+                    <Stack direction='row' spacing={4} align='center'>
+                        <Box fontSize='1.2rem' fontWeight='bold' >test_id</Box>
+                        <Button variant='primary-basic' size='sm' >프로필 편집</Button>
+                        <Button variant='trans-basic' size='md' color='tertiary' leftIcon={<FiLogOut />} fontWeight='bold'>
+                            로그아웃
+                        </Button>
+                    </Stack>
+                    <Flex align='center' justifyContent='space-between'>
+                        <Box color='gray_dark_4' pr='1rem'>
                             게시물 <strong>4</strong>
                         </Box>
-                        <Button variant='ghost' color='gray_dark_4' _hover={{bgColor:'transparent'}}>
+                        <Button variant='trans-basic'>
                             팔로워 <strong>4</strong>
                         </Button>
-                        <Button variant='ghost' color='gray_dark_4' _hover={{bgColor:'transparent'}}>
+                        <Button variant='trans-basic'>
                             팔로잉 <strong>4</strong>
                         </Button>
                     </Flex>
-                    <Button bg='tertiary' color='white'>회원 탈퇴</Button>
+                    <Button variant='tertiary-basic'>회원 탈퇴</Button>
                 </Flex>
             </Flex>
     );
