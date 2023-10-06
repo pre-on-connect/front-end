7c03292022f73ddfa6fbe6554b46bdae061fd76f
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-06 16:49:23 +0900

    Design(사용자 페이지): UserProfileNav 반응형 추가

--- src/components/UserArticleNav/index.tsx
+++ src/components/UserArticleNav/index.tsx
@@ -2,34 +2,55 @@
 import React from 'react';
 import {Divider, Tab, TabIndicator, TabList} from "@chakra-ui/react";
 import {useMediaQuery} from "@mantine/hooks";
+import {MdDashboard} from "@react-icons/all-files/md/MdDashboard";
+import {BsFillBookmarkFill} from "@react-icons/all-files/bs/BsFillBookmarkFill";
+import {BiCommentDetail} from "@react-icons/all-files/bi/BiCommentDetail";
 
 const userArticleNavMenu = [
     {
         id: 1,
         name: '게시물',
-        type: 'posted'
+        type: 'posted',
+        icon: <MdDashboard/>
     },
     {
         id: 2,
         name: '좋아요 한 글',
-        type: 'liked'
+        type: 'liked',
+        icon: <BsFillBookmarkFill/>
     },
     {
         id: 3,
         name: '댓글 단 글',
-        type: 'commented'
+        type: 'commented',
+        icon: <BiCommentDetail/>
     }
 ]
+
 
 function UserArticleNav() {
     const isMobile = useMediaQuery("(max-width: 767px)");
 
+
     return (
         <>
-            <TabList justifyContent='space-around' width='50%'>
+            <TabList justifyContent='space-around'
+                     width={isMobile ? '100%' : '50%'}
+                     height='4rem'
+            >
                 {
+
                     userArticleNavMenu.map(v => {
-                        return <Tab key={v.id} color='gray_dark_1' _selected={{color:'gray_dark_4', fontWeight:500}} fontSize={isMobile ? '.875rem' : '1rem'}>{v.name}</Tab>;
+                        return (
+                            <Tab
+                                key={v.id}
+                                color='gray_dark_1'
+                                _selected={{color: 'gray_dark_4', fontWeight: 500}}
+                                fontSize={isMobile ? '2rem' : '1rem'}
+                            >
+                                {isMobile ? v.icon : v.name}
+                            </Tab>
+                        );
                     })
                 }
             </TabList>
