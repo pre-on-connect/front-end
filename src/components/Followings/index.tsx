2e9be5c1de674e28f9201f54f4a526ff42c5f7e9
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-06 22:16:55 +0900

    Feature: Followers, Followings 컴포넌트 추가

---
+++ src/components/Followings/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Flex, Heading} from "@chakra-ui/react";
+import {SearchBar, UserProfileItem} from "@/components/ui";
+
+function Followings() {
+    return (
+        <Flex maxHeight='30rem' width='20rem' flexDirection='column' align='center' justify='space-between'>
+            <SearchBar placeholder='검색어를 입력하세요'/>
+            <Flex maxHeight='10rem' width='14rem' flexDirection='column' overflow='auto'>
+                <UserProfileItem  isFollowing={true} nickName='사용자1'/>
+                <UserProfileItem  isFollowing={true} nickName='사용자2'/>
+                <UserProfileItem  isFollowing={true} nickName='사용자3'/>
+                <UserProfileItem  isFollowing={true} nickName='사용자4'/>
+            </Flex>
+            <Flex maxHeight='5rem' width='14rem' flexDirection='column'  overflow='auto'>
+                <Heading size='md'>회원 추천</Heading>
+                <Flex flexDirection='column'>
+                    <UserProfileItem isFollowing={true} nickName='사용자1'/>
+                    <UserProfileItem isFollowing={true} nickName='사용자1'/>
+                    <UserProfileItem isFollowing={true} nickName='사용자1'/>
+                </Flex>
+            </Flex>
+        </Flex>
+    );
+}
+
+export default Followings;
