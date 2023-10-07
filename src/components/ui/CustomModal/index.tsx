c1759c18204ca6812735e5d5513734bb122b053d
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-07 18:36:37 +0900

    Fix(공통): CustomModal - props로 모달사이즈 결정하도록 수정

--- src/components/ui/CustomModal/index.tsx
+++ src/components/ui/CustomModal/index.tsx
@@ -7,17 +7,51 @@ export interface CustomModalProps {
     handleModalClose: () => void;
     handleModalOpen?: () => void;
     children?: React.ReactNode;
+    size?:'lg'|'sm';
 }
 
-export const CustomModal = ({isOpen, handleModalClose, children}: CustomModalProps) => {
+const customModalSize = {
+    lg:{
+        wrapper: {
+            base: "full",
+            sm: "sm",
+            md: "xl"
+        },
+        contentW: {
+            md: "700px",
+            lg: "1000px"
+        },
+        contentH:{
+            md:'600px'
+        }
+    },
+    sm:{
+        wrapper: {
+            base: "md",
+            sm: "xs",
+            md: "lg"
+        },
+        contentW: {
+            md: "20rem",
+            lg: "25rem"
+        },
+        contentH:{
+            md:'30rem'
+        }
+    }
+}
+
+export const CustomModal = ({isOpen, handleModalClose, children, size = 'lg'}: CustomModalProps) => {
     const isMobile = useMediaQuery("(max-width: 767px)");
     const isTablet = useMediaQuery("(min-width: 768px)");
     const isDesktop = useMediaQuery("(min-width: 1200px)");
 
+    const modalSize = customModalSize[size];
+
     return (
-        <Modal onClose={handleModalClose} isOpen={isOpen} isCentered size={{base: "full", sm: "sm", md: "xl"}}>
+        <Modal onClose={handleModalClose} isOpen={isOpen} isCentered size={modalSize.wrapper}>
             <ModalOverlay/>
-            <ModalContent maxWidth={{md: "700px", lg: "1000px"}} height={{md: "600px"}}>
+            <ModalContent maxWidth={modalSize.contentW} height={modalSize.contentH}>
                 {isMobile ? (
                     <ModalCloseButton zIndex="10"/>
                 ) : (
