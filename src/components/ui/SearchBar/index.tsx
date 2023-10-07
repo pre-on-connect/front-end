0d059849d827cd417d90059865e293f38f42b157
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-07 20:30:50 +0900

    Fix(공통): SearchBar - 공통으로 사용할 수 있도록 size props 추가

--- src/components/ui/SearchBar/index.tsx
+++ src/components/ui/SearchBar/index.tsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { Input, Image, Flex } from "@chakra-ui/react";
+import {Input, Image, Flex} from "@chakra-ui/react";
 
 interface SearchBarProps {
-  placeholder?: string;
+    placeholder?: string;
+    size?: 'lg' | 'md';
 }
 
-export const SearchBar = ({ placeholder }: SearchBarProps) => {
-  return (
-    <Flex position="relative" width={{ base: "300px", sm: "400px" }} m="0 auto">
-      <Input
-        placeholder={placeholder ? placeholder : "검색어를 입력하세요"}
-        size="md"
-        width={{ base: "300px", sm: "400px" }}
-      />
-      <Image src="/assets/search.svg" alt="" width="20px" position="absolute" right="10px" top="25%" cursor="pointer" />
-    </Flex>
-  );
+const customSearchBarSize = {
+    'lg': {base: "300px", sm: "400px"},
+    'md': {base: "300px", sm: "360px", md:'300px'}
+}
+
+export const SearchBar = ({placeholder, size = 'lg'}: SearchBarProps) => {
+    return (
+        <Flex position="relative" width={customSearchBarSize[size]} m="0 auto">
+            <Input
+                placeholder={placeholder ? placeholder : "검색어를 입력하세요"}
+                size="md"
+                width={customSearchBarSize[size]}
+            />
+            <Image src="/assets/search.svg" alt="" width="20px" position="absolute" right="10px" top="25%"
+                   cursor="pointer"/>
+        </Flex>
+    );
 };
